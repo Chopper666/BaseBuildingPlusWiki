88904b09f4406042dfff16d43c2b255b36dc3cd1
mrw00kie <karlfried3@yahoo.de>
2019-04-19 17:42:08 +0200

    Create changelog.cpp

---
+++ changelog.cpp
@@ -0,0 +1,37 @@
+   ______________________________
+ / \                             \.
+|   |                            |.
+ \_ |     BASE BUILDING PLUS     |.
+    |                            |.
+    |         CHANGELOG          |.
+    |                            |.
+    |   - Authors                |.
+    |         Hennessy           |.
+    |         Chopper            |.
+    |         W00kie             |.
+    |   _________________________|___
+    |  /                            /.
+    \_/dc__________________________/.
+    
+    Update: 16. Apr. 23:12 Uhr
+      Added concrete slot to the workbench.
+    
+    Update: 15. Apr. 20:33
+      Fixed workbench recipe. Now 1 log and 5 planks.
+    
+    Update: 15. Apr. 20:12
+      Fixed workbench recipe. Now 1 log and 5 planks.
+    
+    Update: 14. Apr. 21:40
+      Re-sized all base parts to a universal size. Placement and snapping should be much better now.
+      Added missing metal upgrade options to full frame and half frame. Temporary fix: Use a Hatchet to
+      upgrade the half frame to metal and a Hammer to upgrade to wood.
+      Fixed workbench spawning on it's side.
+      2 missing items added to types.xml (the last 2 in the file, BBP_Simple_Floor_Hatch_Frame and BBP_Simple_Floor_Hatch_Stairs).
+    
+    Update: 13. Apr. 6:43
+    
+    Update: 13. Apr. 6:42
+      
+    Update: 13. Apr. 6:41
+      Early BETA release, expect a lot of bugs :D
